c1dae6b0f64d543ae79a597640370fc5fb0da677
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-12-04 13:10:00 +0700

    move aos init to App.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,9 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'
 import { useEffect } from 'react';
 import MyProfile from '../Components/MyProfile';
 import Projects from '../Components/Projects';
 
 function Home() {
   useEffect(() => {
-    AOS.init();
     document.title = "Home | AFR Web Profile";
   });
 
