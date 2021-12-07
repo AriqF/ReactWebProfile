2fc928db13caab4643978a6f9ca2d58c147fbf99
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-12-07 19:31:06 +0700

    gallery image

---
+++ src/assets/img/Projects/ManGo/index.js
@@ -0,0 +1,14 @@
+export {default as about} from './about.png';
+export {default as adminQuizList} from './adminQuizList.png';
+export {default as dashboardAdmin} from './dashboardAdmin.png';
+export {default as dashboardUser} from './dashboardUser.png';
+export {default as hiragana} from './hiragana.png';
+export {default as indexPage} from './index.png';
+export {default as listUser} from './listUser.png';
+export {default as login} from './login.png';
+export {default as profile} from './profile.png';
+export {default as quiz} from './quiz.png';
+export {default as quizList} from './quizList.png';
+export {default as ratingList} from './ratingList.png';
+export {default as register} from './register.png';
+export {default as result} from './result.png';
