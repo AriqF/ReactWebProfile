ef6cd05da581c9f12d1024f5d552292599441a02
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-12-07 19:39:29 +0700

    Update ManGoDet.js

--- src/Pages/ProjectDetails/ManGoDet.js
+++ src/Pages/ProjectDetails/ManGoDet.js
@@ -22,18 +22,15 @@ function ManGoDet() {
                             In this project i took the role as Front-End Engineer and web content writer. This project also managed 
                             by scrum using Notion as the tools for increasing productivity and efficiency. 
                         </p>
-                        <div class="collapse" id="collapseExample">
-                            <div class="card card-body">
-                                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
-                            </div>
-                        </div>
+
                         
                         <div className="buttonContainer">
                             <a href="https://github.com/AriqF/man-go"
                             variant="custom" className="btnr btnr-red"><i class="fab fa-github"></i> Git Repository</a>  
 
-                            <Button type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"
+                            {/* <Button type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"
                             variant="custom" className="btnr btnr-trans"><i class="fas fa-book"></i> Library Details</Button>  
+                            */}
                         </div>
                         
                     </div>
